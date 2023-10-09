b999b53ddbedb66ee6feda29b770f521a74df470
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-10-09 19:27:26 +0600

    Module #1. Task #2. Build Tools. Maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+
+        for (String e : args) {
+            if (!StringUtils.isPositiveNumber(e)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,23 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        boolean isPositive = false;
+
+        if (str == null) {
+            return isPositive;
+        }
+
+        if (str.startsWith("0")) {
+            return isPositive;
+        }
+
+        if (NumberUtils.toDouble(str) > 0) {
+            isPositive = true;
+        }
+
+        return isPositive;
     }
 }
